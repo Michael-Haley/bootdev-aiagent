1d7f03fb54ace2ed7787cede47e729cdc3d93e94
mhaley7 <haley@dxc.com>
2026-09-14 20:25:45 +0100

    Formatting changes with ruff

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -10,10 +10,15 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
         # Will be True or False
-        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+        )
 
         if valid_target_dir == False:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+            return (
+                f'Error: Cannot list "{directory}" as it is outside '
+                "the permitted working directory"
+            )
 
         return f'Success: "{directory}" is within the working directory'
     except (OSError, TypeError, ValueError):
